e092086b1b7eb7a3f3b33f4aea7f68034c8433eb
Necrosan <necrosan@gmail.com>
2017-12-29 17:52:05 -0500

    Completion of step 2 in the exercise.

--- randgen.py
+++ randgen.py
@@ -2,8 +2,26 @@
 
 import random
 
-def rand_gen():
-	numbers = [1, 2, 3, 4, 5]
-	prob = [0.5, 0.25, 0.15, .05, .05]
-	random.seed()
-	print(random.choices(numbers, weights=prob, k=1).pop())
+class RGen():
+	def __init__(self):
+		self._history = []
+		self._numbers = [1, 2, 3, 4, 5]
+		self._prob = [0.5, 0.25, 0.15, .05, .05]
+		random.seed()
+
+	def _rand_gen(self):
+		return (random.choices(self._numbers, weights=self._prob, k=1).pop())
+
+	def random(self):
+		num = self._rand_gen()
+		self._history.insert(0, num)
+		if len(self._history) > 100:
+			self._history.pop()
+		return num
+
+	def freq(self, *args):
+		nums = args if args else range(1,6)
+		return ['{} = {}%'.format(n, int(round((self._history.count(n) / len(self._history)) * 100))) for n in nums]
+
+	def run(self):
+		print(self.random())
